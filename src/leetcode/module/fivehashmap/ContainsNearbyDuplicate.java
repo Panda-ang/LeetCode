4ef9454d04da29d5c87d12478e0adc2f0762fe50
gaoyan <770695872@qq.com>
2025-02-28 16:33:24 +0800

    test

--- src/leetcode/module/fivehashmap/ContainsNearbyDuplicate.java
+++ src/leetcode/module/fivehashmap/ContainsNearbyDuplicate.java
@@ -10,10 +10,7 @@ import java.util.Map;
 public class ContainsNearbyDuplicate {
     public static void main(String[] args) {
         containsNearbyDuplicate2(new int[]{1,2,3,1}, 3);
-
-
         containsNearbyDuplicate(new int[]{1,0,1,1}, 3);
-
     }
 
     //方法一：枚举右，维护左
